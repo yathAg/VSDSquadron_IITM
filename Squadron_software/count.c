d2866c76d4467b84d7b01a18af76ff7c5b0c63a8
Dhinesh <64545984+BalaDhinesh@users.noreply.github.com>
2023-04-13 07:44:59 +0530

    delete delay.h

--- Squadron_software/count.c
+++ Squadron_software/count.c
@@ -1,5 +1,11 @@
-#include "delay.h"
 #include <stdio.h>
+#include <time.h>
+
+void delay(int n) {
+    int us = n; // microseconds
+    clock_t start_time = clock();
+    while (clock() < start_time + (us * CLOCKS_PER_SEC / 1000000));
+}
 
 void display(int count) // Function to display the data on the four Led's
 {
--- Squadron_software/delay.h
+++
@@ -1,7 +0,0 @@
-#include <time.h>
-
-void delay(int n) {
-    int us = n; // microseconds
-    clock_t start_time = clock();
-    while (clock() < start_time + (us * CLOCKS_PER_SEC / 1000000));
-}
